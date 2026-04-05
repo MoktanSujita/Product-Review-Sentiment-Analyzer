748245adf20b625861bd5505eacb5bf71194c14e
MoktanSujita <sujitamoktan77@gmail.com>
2026-04-05 23:02:10 +0545

    Add product input form and fake sentiment analysis

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [  
-    path('', views.reviews_list, name='reviews_list'), 
+    path('', views.home, name='home'), 
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-from .models import Review  
-
+from textblob import TextBlob
 # Create your views here.
-def reviews_list(request):
-    reviews = Review.objects.all()
-    return render(request, 'reviews_list.html', {'reviews': reviews})
+def home(request):
+    result = None
+    sentiment = None
+
+    if request.method == 'POST':
+        url = request.POST.get('url')
+
+        fake_reviews =[
+            "This product is amazing! I love it.",
+            "Very bad quality, not worth the money.",
+            "I am extremely satisfied with this purchase.",
+        ]   
+
+        combined_text = "".join(fake_reviews)
+        blob =TextBlob(combined_text)
+        polarity = blob.sentiment.polarity
+        if polarity > 0:
+            sentiment = "Positive"
+        elif polarity < 0:
+            sentiment = "Negative"
+        else:
+            sentiment = "Neutral"
+
+            result = f"Polarity Score: {polarity}"
+    return render(request, "reviews_list.html",{
+            "result" : result,
+            "sentiment" : sentiment 
+        })
